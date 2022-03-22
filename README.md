98853030a7b28d4c02d9f0586d118c896fa7f14c
nathanSamosa <nathanthesamosa@gmail.com>
2022-03-22 11:10:12 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Larbert Tutors
---
+++ src/client/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+        <h1>Hello, world!</h1>
+    </div>
+  );
+}
